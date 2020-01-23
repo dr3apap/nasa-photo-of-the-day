3cfc5d1dfc1e63a3e60022e97bbbd0c12a15a1ac
Adebola  Adesina <adebolaadesina@adebolas-mbp.home>
2020-01-22 16:55:54 -0800

    Image displayed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios"
 import "./App.css";
+import DisplayImg from "./components/DisplayImg"
 
 function App() {
+  const [nasa, setNasa] = useState([]);
+  useEffect(() => {
+    axios.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2020-01-22")
+      .then(res => {
+        setNasa(res.data)
+        console.log(res.data)
+      })
+
+      .catch(error => {
+        console.log("the data was requested", error)
+      })
+
+  }, [])
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      <DisplayImg data={nasa} />
     </div>
   );
 }
