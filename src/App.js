10bef5435cccc68b4a009ee1de94b8a009d59ecc
Adebola  Adesina <adebolaadesina@adebolas-mbp.home>
2020-01-23 12:50:42 -0800

    added eventhandler for date-change

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios"
 import "./App.css";
+import DateInput from "./components/DateInput"
 import DisplayImg from "./components/DisplayImg"
 
 function App() {
-  const [nasa, setNasa] = useState([]);
+  let [nasa, setNasa] = useState([]);
+  let [date, setDate] = useState("");
+
+  const changeDate = e => {
+    e.preventDefault()
+    let inputFromDate = e.target[0].value
+    setDate(date + inputFromDate)
+
+    console.log(e.target)
+  }
+
+  const DateInput = props => (
+    <form onSubmit={props.changeDate}>
+      Enter a Date (YYYY-MM-DD):
+    <input />
+      <input type="submit" />
+    </form>
+
+  );
+
+
+
+
+
   useEffect(() => {
-    axios.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2020-01-22")
+    axios.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date={query}")
       .then(res => {
         setNasa(res.data)
         // console.log(res.data)
       })
 
       .catch(error => {
-        // console.log("the data was requested", error)
+        console.log("the data was requested", error)
       })
 
   }, [])
 
   return (
     <div className="App">
+      <DateInput changeDate={changeDate} />
       <DisplayImg data={nasa} />
     </div>
   );
---
+++ src/components/DateInput.js
@@ -0,0 +1,9 @@
+import React from "react"
+export default function DateInput() {
+
+    return <div>DateInput:</div>
+
+
+
+
+}
